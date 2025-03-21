2651d9ee80133d630d7caf4a1663281a36cf590e
klarabeatryz <klaraestudos1@gmail.com>
2025-03-21 14:19:23 +0000

    atividade01

---
+++ atividade01-tiposdados/questao01.js
@@ -0,0 +1,10 @@
+let n1 = 5;
+let n2 = 4;
+let n3 = 3;
+let n4 = 2;
+let n5 = 1;
+
+let eA = "Aprendi";
+let jS = "JavaScript";
+
+console.log(n1, "," , n2, "," , n3, "," , n4, "," , n5, "..." , eA, jS , "!!!"); 
